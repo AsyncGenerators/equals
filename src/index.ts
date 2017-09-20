70cf4cf9e6a23d5fe292d4f23698286277cf0d1b
Meirion Hughes <crakinshot@yahoo.com>
2017-09-20 23:29:42 +0100

    refactor: use @async-generator/iterable

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
+import iterable from '@async-generators/iterable';
+
 export default async function equal<T>(
   first: AsyncIterable<T> | Iterable<T>,
   second: AsyncIterable<T> | Iterable<T>,
   comparer: (a: T, b: T) => boolean
     = function (a, b) { return a === b }
 ): Promise<boolean> {
-
-  const ItA = first[Symbol.asyncIterator] || first[Symbol.iterator]
-  const ItB = second[Symbol.asyncIterator] || second[Symbol.iterator]
-
-  if (typeof ItA !== "function")
-    throw Error("first parameter is not iterable");
-  if (typeof ItB !== "function")
-    throw Error("second parameter is not iterable");
-
-  const ita: AsyncIterator<T> | Iterator<T> = ItA.call(first);
-  const itb: AsyncIterator<T> | Iterator<T> = ItB.call(second);
+  
+  const ita = iterable(first, "first parameter is not iterable")[Symbol.asyncIterator]();
+  const itb = iterable(second, "second parameter is not iterable")[Symbol.asyncIterator]();
 
   while (true) {
 
